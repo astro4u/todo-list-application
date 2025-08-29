ff7f565b5a5c9020dd38d1178025fb889679d356
Cameron Tangco <cctangco@gmail.com>
2025-08-29 17:45:46 -0400

    fixed relative file path for an asset

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,7 +12,7 @@ export function Header({ child1, child2 }) {
     return (
         <div className={styles.header}>
             <div className={styles.todolisttitle}>
-                <img src="src\assets\UpdateListIt.png" alt="ListIt logo"/>
+                <img src=".\src\assets\UpdateListIt.png" alt="ListIt logo"/>
                 {/*<p className={styles.title}>L<span className="text-veryImportant">!</span>st <span className="text-semiImportant">!</span>t</p>*/}
             </div>
             <div className={styles.addtaskbuttonarea}>
